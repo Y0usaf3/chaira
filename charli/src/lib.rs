e073748e6554b5c4538431dffa5e9826a41935b6
Youssef Lmouden <youssef.pro.coding@gmail.com>
2026-05-07 18:05:21 +0100

    charli is done :pf:

--- charli/src/lib.rs
+++ charli/src/lib.rs
@@ -1,14 +1,9 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+//use models::*;
+use surrealdb_types::SurrealValue;
+use surrealism::surrealism;
+
+#[surrealism]
+pub fn can(mask: u32, flag: u32) -> bool {
+    (mask & 1) == 1 || (mask & flag) == flag
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}

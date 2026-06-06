1adb72b76690cd222282b9d237e33ac4b3c1372c
Yousafe <youssef.pro.coding@gmail.com>
2026-06-06 18:06:18 +0100

    verify and convert to function for value

--- models/src/record/cell/value.rs
+++ models/src/record/cell/value.rs
@@ -21,7 +21,7 @@ use super::roll_up_value::RollUpValue;
 use super::single_line_value::SingleLineValue;
 use super::url_value::UrlValue;
 use crate::kinds::FieldConfig;
-use crate::prelude::*;
+use crate::{ValueType, prelude::*};
 
 #[derive(Debug, Clone, SurrealValue, PartialEq, Eq, Hash, serde::Serialize, serde::Deserialize)]
 pub enum Value {
@@ -49,7 +49,65 @@ pub enum Value {
 }
 
 impl Value {
-    pub fn verify(&self, _config: FieldConfig) -> Result<(), ValueError> {
-        Ok(())
+    pub fn verify(&self, config: FieldConfig) -> Result<(), ValueError> {
+        match self {
+            Self::SingleLine(v) => v.verify(config),
+            Self::LongText(v) => v.verify(config),
+            Self::Email(v) => v.verify(config),
+            Self::URL(v) => v.verify(config),
+            Self::Phone(v) => v.verify(config),
+            Self::Number(v) => v.verify(config),
+            Self::Decimal(v) => v.verify(config),
+            Self::Currency(v) => v.verify(config),
+            Self::Percent(v) => v.verify(config),
+            Self::Rating(v) => v.verify(config),
+            Self::Date(v) => v.verify(config),
+            // Self::Duration(v) => v.verify(config), // we still gonna support those types on day
+            // :p
+            // Self::Link(v) => v.verify(config),
+            // Self::LookUp(v) => v.verify(config),
+            // Self::RollUp(v) => v.verify(config),
+            // Self::Formula(v) => v.verify(config),
+            // Self::AutoNumber(v) => v.verify(config),
+            // Self::CreatedAt(v) => v.verify(config),
+            // Self::ModifiedTime(v) => v.verify(config),
+            // Self::Attachment(v) => v.verify(config),
+            // Self::JSON(v) => v.verify(config),
+            _ => Err(ValueError::Unknown),
+        }
+    }
+
+    pub fn convert_to(&self, target_config: &FieldConfig) -> Result<Value, ValueError> {
+        match self {
+            Self::SingleLine(v) => v.convert_to(target_config),
+            Self::LongText(v) => v.convert_to(target_config),
+            Self::Email(v) => v.convert_to(target_config),
+            Self::URL(v) => v.convert_to(target_config),
+            Self::Phone(v) => v.convert_to(target_config),
+            Self::Number(v) => v.convert_to(target_config),
+            Self::Decimal(v) => v.convert_to(target_config),
+            Self::Currency(v) => v.convert_to(target_config),
+            Self::Percent(v) => v.convert_to(target_config),
+            Self::Rating(v) => v.convert_to(target_config),
+            Self::Date(v) => v.convert_to(target_config),
+            // Self::Duration(v) => v.convert_to(target_config),
+            // Self::Link(v) => v.convert_to(target_config),
+            // Self::LookUp(v) => v.convert_to(target_config),
+            // Self::RollUp(v) => v.convert_to(target_config),
+            // Self::Formula(v) => v.convert_to(target_config),
+            // Self::AutoNumber(v) => v.convert_to(target_config),
+            // Self::CreatedAt(v) => v.convert_to(target_config),
+            // Self::ModifiedTime(v) => v.convert_to(target_config),
+            // Self::Attachment(v) => v.convert_to(target_config),
+            // Self::JSON(v) => v.convert_to(target_config),
+            _ => Err(ValueError::Unknown),
+        }
     }
 }
+
+// alr so abt the field version thingy idk, what we can do is that we set a version of the type,
+// live version = 1, and it always uses the current type, but when its version = 2 now, and there is
+// a version 1 cell, we apply a mogration code to automaticly migrate the cell from version 1 to
+// version 2, so it always handles the latest type, since all the type code is well, on the server
+// side :p
+// oki so lemem read what i was doing lately abt the field thing

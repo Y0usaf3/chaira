44ddbb0dae2dbd5d94f1c11d70d8f3efb3a83d22
Yousafe <youssef.pro.coding@gmail.com>
2026-06-06 16:27:19 +0100

    convertion code for currency value

--- models/src/record/cell/currency_value.rs
+++ models/src/record/cell/currency_value.rs
@@ -1,4 +1,6 @@
-use crate::{OrderedFloatIThink, Value, ValueError, ValueType, kinds::FieldConfig, prelude::*};
+use crate::{
+    LongTextValue, OrderedFloatIThink, Value, ValueError, ValueType, kinds::FieldConfig, prelude::*,
+};
 use ordered_float::OrderedFloat;
 
 #[derive(Debug, Clone, SurrealValue, PartialEq, Eq, Hash, serde::Serialize, serde::Deserialize)]
@@ -22,7 +24,21 @@ impl ValueType<OrderedFloatIThink> for CurrencyValue {
         try_convert!(target_config  {
             Text {
                 SingleLine { default, max_length } => {
-                    Value::SingleLine(crate::SingleLineValue { value: self.value.0.to_string() })
+                    Value::SingleLine(crate::SingleLineValue { value: self.value.0.to_string().chars().take(*max_length as usize).collect() })
+                };
+                LongText { rich_text } => {
+                    Value::LongText(Box::new(LongTextValue { value: self.value.0.to_string() }) )
+                }
+            };
+            Number {
+                Number { default } => {
+                    Value::Number(crate::NumberValue { value: self.value.0.round() as isize })
+                };
+                Decimal { precision, default } => {
+                    Value::Decimal(crate::DecimalValue { value: self.value.clone() })
+                };
+                Percent { show_bar, precision } => {
+                    Value::Percent(crate::PercentValue { value: self.value.0.round() as i32 })
                 }
             };
         })

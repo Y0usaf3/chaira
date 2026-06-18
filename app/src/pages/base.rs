c612ab0b37791b05daf30e322490adf1899afbfb
Yousafe <youssef.pro.coding@gmail.com>
2026-06-18 14:59:54 +0100

    aaa just testing

--- app/src/pages/base.rs
+++ app/src/pages/base.rs
@@ -1,6 +1,9 @@
 use leptos::prelude::*;
+use leptos_router::hooks::use_params_map;
 
 #[component]
 pub fn BasePage() -> impl IntoView {
-    view! {}
+    let params = use_params_map();
+    let id = params.read().get("id");
+    view! { <p>{format!("{id:?}")}</p> }
 }

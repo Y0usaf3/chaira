5a63817e12003ab1b91bb7fea3d107834de9e3f3
Yousafe <youssef.pro.coding@gmail.com>
2026-06-19 10:55:31 +0100

    aaaa lets try this

--- package.nix
+++ package.nix
@@ -61,7 +61,7 @@ in
 
       buildPhaseCargoCommand = ''
         export HOME=$(mktemp -d)
-        cargo-leptos build --release
+        CARGO_LEPTOS_WASM_BINDGEN=$(command -v wasm-bindgen) cargo-leptos build --release
       '';
 
       installPhase = ''
